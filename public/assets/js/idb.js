ca8f5d7067af3003c7cf8edf8cbafedb9ce8d903
Neil Dino <N.dino90@gmail.com>
2020-09-25 17:20:24 -0600

    offline save and add working

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -29,61 +29,60 @@ request.onerror = function (event) {
     console.log(event.target.errorCode);
 }
 
-// This function will be executed if we attempt to submit a new pizza and there's no internet connection
+/// This function will be executed if we attempt to submit a new pizza and there's no internet connection
 function saveRecord(record) {
     // open a new transaction with the database with read and write permissions 
-    console.log('formdata', FormData)
-    
     const transaction = db.transaction(['new_pizza'], 'readwrite');
-
+  
     // access the object store for `new_pizza`
     const pizzaObjectStore = transaction.objectStore('new_pizza');
-
+  
     // add record to your store with add method
-    pizzaObjectStore.add(record)
-    console.log('record', record)
-}
+    pizzaObjectStore.add(record);
+  }
 
-function uploadPizza() {
+  function uploadPizza() {
     // open a transaction on your db
-    const transaction = db.transaction(['new_pizza']);
-
+    const transaction = db.transaction(['new_pizza'], 'readwrite');
+  
     // access your object store
     const pizzaObjectStore = transaction.objectStore('new_pizza');
-
+  
     // get all records from store and set to a variable
     const getAll = pizzaObjectStore.getAll();
-
-    getAll.onsuccess = function () {
+  
+   // upon a successful .getAll() execution, run this function
+    getAll.onsuccess = function() {
         // if there was data in indexedDb's store, let's send it to the api server
         if (getAll.result.length > 0) {
-            fetch('/api/pizzas', {
-                method: 'POST',
-                body: JSON.stringify(getAll.result),
-                Headers: {
-                    Accept: 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json'
-                }
-            })
+        fetch('/api/pizzas', {
+            method: 'POST',
+            body: JSON.stringify(getAll.result),
+            headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+            }
+        })
             .then(response => response.json())
             .then(serverResponse => {
-                if (serverResponse.message) {
-                    throw new Error(serverResponse)
-                }
-                // open one more transaction
-                const transaction = db.transaction(['new_pizza'], 'readwrite');
-                //access the new_pizza object store
-                pizzaObjectStore.clear();
+            if (serverResponse.message) {
+                throw new Error(serverResponse);
+            }
+            // open one more transaction
+            const transaction = db.transaction(['new_pizza'], 'readwrite');
+            // access the new_pizza object store
+            const pizzaObjectStore = transaction.objectStore('new_pizza');
+            // clear all items in your store
+            pizzaObjectStore.clear();
 
-                alert('All saved pizza has been submitted!')
+            alert('All saved pizza has been submitted!');
             })
             .catch(err => {
-                console.log(err)
+            console.log(err);
             });
-        
         }
     };
-}
+  }
 
 
 
